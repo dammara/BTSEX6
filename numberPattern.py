08d7d130db1020836c3ad17a263885db9fa3a6fc
dammara <56162593+dammara@users.noreply.github.com>
2021-09-15 09:39:13 -0400

    Add files via upload

---
+++ numberPattern.py
@@ -0,0 +1,37 @@
+# Markhus Dammar
+# 15 September 2021
+# EXERCISE 6: THis pattern has color and numbers, using formatting to make the pattern
+
+
+a = "1"
+b = "2"
+c = "3"
+d = "4"
+
+
+print(f"""
+\033[34m     {a}
+  {a}     {a}
+{a}         {a}
+  {a}  {a}  {a}
+   {a}   {a}
+\033[31m     {b}
+  {b}     {b}
+{b}         {b}
+  {b}  {b}  {b}
+   {b}   {b}
+\033[34m     {c}
+  {c}     {c}
+{c}         {c}
+  {c}  {c}  {c}
+   {c}   {c}
+\033[31m     {d}
+  {d}     {d}
+{d}         {d}
+  {d}  {d}  {d}
+   {d}   {d}
+
+
+
+
+""")
